798e924192d735d1a18527993c9408491f4fd457
Saket Verma <saket3324verma@gmail.com>
2021-09-28 01:20:44 +0530

    Commented Edit code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,31 +20,40 @@ export class LoginComponent implements OnInit {
   }
   constructor(private router: Router, private service: NotesServiceService) {
     this.screen.width = screen.width;
-   }
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    let [email, password] = [localStorage.getItem("Email"), localStorage.getItem("Password")];
+    if (email && email.length > 0 && password && password.length > 0) {
+      this.router.navigate(["/notes"]);
+      this.checkLogin({ email: email, password: password });
+    } else {
+      this.router.navigate(["/"]);
+    }
+  }
 
   checkLogin(event) {
-    event.preventDefault();
+    // event.preventDefault();
     this.loginChecked = true;
+    let [email, password] = [localStorage.getItem("Email"), localStorage.getItem("Password")];
+
+
     this.service.loginData.default.data.forEach(element => {
       if (this.email === element.email) {
         if (this.password === element.password) {
           this.isLoginValid = true;
           this.service.setCurrentLogin(element);
           this.router.navigate(["/notes"]);
-          
+         
         } else {
-          this.errorString = "||    INVALID CREDENTIALS     ||"; 
+          this.errorString = "||    INVALID CREDENTIALS     ||";
           this.isLoginValid = false;
         }
       } else {
-          this.errorString = "||     INVALID CREDENTIALS     ||";
-          this.isLoginValid = false;
-        }
+        this.errorString = "||     INVALID CREDENTIALS     ||";
+        this.isLoginValid = false;
       }
-    );
-
+    });
   }
 
   mailChanged(event) {
